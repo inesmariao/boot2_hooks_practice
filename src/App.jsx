62135e10075e4d1636ef772a73eeabfe8d6d6a41
inesmariao <inesmao.misena@gmail.com>
2024-07-08 07:12:50 -0500

    UseReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { UseReducer } from './components/UseReducer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,6 +23,7 @@ function App() {
         </button>
         <hr />
       </div>
+      <UseReducer count={count} setCount={setCount} />
     </>
   )
 }
